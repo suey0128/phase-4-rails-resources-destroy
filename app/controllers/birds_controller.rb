8ce3bb0da01b5bb2fe6fdd36b55eb5f7979dc49d
suey0128 <syui0128@gmail.com>
2021-07-18 23:55:53 -0700

    commit tree 2163463e92ab495eeac48a10392a1c3dfeede570

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -44,6 +44,19 @@ class BirdsController < ApplicationController
     end
   end
 
+  def destroy
+    #find the bird
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      #remove it 
+      bird.destroy
+      #(send a empty response) response: 
+      head :no_content
+    else
+      render json: {error: "Bird not found"}, status: :not_found
+    end
+  end
+
   private
 
   def bird_params
